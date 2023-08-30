4fac59f78e7b914cde3b328c3874f191b255baff
root <root@DESKTOP-DLAKF8V>
2023-08-30 13:55:42 -0300

    Initial files

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\{
+    UserController
+};
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/users', [UserController::class, 'index']) -> name('users.index');
+Route::get('/users/{id}', [UserController::class, 'show']) -> name('users.show');
+
+Route::get('/', function () {
+    return view('welcome');
+});
